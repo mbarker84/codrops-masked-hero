773cbacbe6e3c9f8eb4bfd4c836b907f0c801af1
Michelle Barker <mbarker_84@hotmail.co.uk>
2021-04-25 23:13:18 +0100

    Add parcel bundler and text animation

--- js/index.js
+++ js/index.js
@@ -1,43 +1,61 @@
-(function () {
-  const hero = document.querySelector("[data-hero]");
+import gsap from "gsap";
+import Splitting from "splitting";
+import throttle from "lodash.throttle";
 
-  /* Menu */
-  const menuButton = document.querySelector('[data-btn="menu"]');
-  const menu = document.querySelector("[data-menu]");
+const hero = document.querySelector("[data-hero]");
 
-  menuButton.addEventListener("click", () => {
-    menu.classList.toggle("is-open");
-    menuButton.classList.toggle("is-active");
+/* Menu */
+const menuButton = document.querySelector('[data-btn="menu"]');
+const menu = document.querySelector("[data-menu]");
+
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
+  menuButton.classList.toggle("is-active");
+});
+
+/* Cursor */
+const onMouseMove = (e) => {
+  const { clientX, clientY } = e;
+  const x = Math.round((clientX / window.innerWidth) * 100);
+  const y = Math.round((clientY / window.innerHeight) * 100);
+
+  gsap.to(hero, {
+    "--x": `${x}%`,
+    "--y": `${y}%`,
+    duration: 0.3,
+    ease: "sine.out",
   });
+};
 
-  /* Timeline */
-  const tl = gsap.timeline({ delay: 1 });
+window.addEventListener("mousemove", throttle(onMouseMove, 30));
 
-  tl.to(hero, {
+/* Text animation */
+Splitting();
+
+// Set initial text styles (before animation)
+gsap.set(".hero--primary .char", {
+  opacity: 0,
+  y: 25,
+});
+
+/* Timeline */
+const tl = gsap.timeline({ delay: 1 });
+
+tl.to(".hero--primary .char", {
+  opacity: 1,
+  y: 0,
+  duration: 0.75,
+  stagger: 0.1,
+})
+  .to(hero, {
     "--maskSize1": "20%",
     duration: 0.5,
     ease: "back.out(2)",
-  }).to(hero, {
+  })
+  .to(hero, {
     "--maskSize2": "28%",
     "--maskSize3": "calc(28% + 0.1rem)",
     duration: 0.5,
     delay: 0.3,
     ease: "back.out(2)",
   });
-
-  const onMouseMove = (e) => {
-    const { clientX, clientY } = e;
-    const x = Math.round((clientX / window.innerWidth) * 100);
-    const y = Math.round((clientY / window.innerHeight) * 100);
-
-    gsap.to(hero, {
-      "--x": `${x}%`,
-      "--y": `${y}%`,
-      duration: 0.3,
-      ease: "sine.out",
-    });
-  };
-
-  /* Cursor */
-  window.addEventListener("mousemove", throttle(onMouseMove, 10));
-})();
