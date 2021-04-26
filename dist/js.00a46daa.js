a0cbbfd120ab70c583c1811b05f739ea827b787d
Michelle Barker <mbarker_84@hotmail.co.uk>
2021-04-26 22:31:35 +0100

    Fix Safari bug and add demo title

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -6647,7 +6647,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52216" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53175" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
